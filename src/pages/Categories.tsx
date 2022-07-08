de3aad3ac719e09906148bee5bce05aa81ec4d65
Sergey <leafysprojects@gmail.com>
2022-07-09 00:58:43 +0300

    TOS-23: create prettier config file

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const Categories = (): JSX.Element => {
-  return (
-    <div>Categories</div>
-  )
-}
+  return <div>Categories</div>;
+};
 
-export default Categories
+export default Categories;
--- src/pages/Random.tsx
+++ src/pages/Random.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const Random = (): JSX.Element => {
   return <div>Random</div>;
