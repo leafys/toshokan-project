75e54c6e03716cfcce32b59c0086356956a1c47e
unknown <dffuqp@gmail.com>
2022-07-24 09:19:47 +0300

    TOS-48: add type for pagination

--- src/components/SeacrhCardsBlock/index.tsx
+++ src/components/SeacrhCardsBlock/index.tsx
@@ -1,6 +1,7 @@
 import CardsBlock from '@components/CardsBlock';
 import {
   IError,
+  ITitlePagination,
   ITopAndUncomingTitle,
 } from '@interfaces/ITopAndUpcomingTitles';
 import { axios } from '@plugins';
@@ -19,7 +20,7 @@ const SeacrhCardsBlock = (): JSX.Element => {
     (searchParams.keys().next().value ?? undefined) ||
     localStorage.getItem('selectedСategory');
   const [searchData, setSearchData] = useState<ITopAndUncomingTitle[]>([]);
-  const [pages, setPages] = useState<any>(null);
+  const [pages, setPages] = useState<ITitlePagination | null>(null);
   const [currentPage, setCurrentPage] = useState<number>(1);
 
   const { ref: observerRef, inView: myElemVisible } = useInView({
@@ -63,10 +64,14 @@ const SeacrhCardsBlock = (): JSX.Element => {
     }
   );
 
+  console.log(pages?.has_next_page);
+
   return (
     <div className="container">
       <div className="pb-4 flex gap-1 items-center">
-        <h1 className="text-xxl font-bold">{t('search_block.title')}</h1>
+        <h1 className="text-xxl font-bold lessMd:text-lg">
+          {t('search_block.title')}
+        </h1>
         <div className="w-[50px] m-w-[75px] h-full bg-purple rounded-full flex items-center justify-center text-white text-base">
           {pages?.items.total}
         </div>
